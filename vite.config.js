d202292d45e9d5c700bf565ff7d50d0beae2e037
bennyhe <hesiyuan009@gmail.com>
2022-09-07 10:42:39 +0800

    feat: 配置脚手架（sass&eslint&autoprefixer等）

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,54 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: [
+            'ie >= 11',
+            'Android >= 5',
+            'iOS >= 9'
+          ],
+          grid: true
+        })
+      ]
+    }
+  },
+  server: {
+    port: 3104,
+    cros: true,
+    open: true
+  },
+  // base: 'digimon/',
+  base: 'xmindWork/',
+  resolve: {
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'node_lib'
+          }
+        }
+      }
+    }
+  }
 })
